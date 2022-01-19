21d12c4d4f0d3ef1e11e317e7a3960f20ec211cc
CelalMirkan <mirkan.albayrak@gmail.com>
2022-01-19 14:02:32 +0100

    commit first

---
+++ src/data.js
@@ -0,0 +1,69 @@
+const data = {
+
+    products: [
+
+        {
+            _id: '1',
+            model: 'Air Jordan 1 Mid',
+            category: 'High Top',
+            image: '/images/p1.jpg',
+            price: 159.99,
+            countInStock: 20,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 22,
+            description: 'The new Air Jordan 1 Mid in Grey ' 
+        },
+        {
+            _id: '2',
+            model: 'Ultra Boost 1.0',
+            category: 'Running',
+            image: '/images/p2.jpg',
+            price: 179.99,
+            countInStock: 13,
+            brand: 'Adidas',
+            rating: 5,
+            numReviews: 300,
+            description: 'The Lifestyle running shoe' 
+        },
+        {
+            _id: '3',
+            model: 'Yeezy Boost 350 V2',
+            category: 'Low Top',
+            image: '/images/p3.jpg',
+            price: 220,
+            countInStock: 1,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 1111,
+            description: 'Kanyes newest creation' 
+        },
+        {
+            _id: '4',
+            model: 'Air Jordan 1 High OG Royal Blue',
+            category: 'High Top',
+            image: '/images/p4.jpg',
+            price: 179.99,
+            countInStock: 0,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 22,
+            description: 'The new Air Jordan 1 High in the sickest blue imaginable' 
+        },
+        {
+            _id: '5',
+            model: 'Nike x Parra Air Max 1',
+            category: 'low Top',
+            image: '/images/p5.jpg',
+            price: 129.99,
+            countInStock: 44,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 182,
+            description: 'The first collaboration between Nike and Parra' 
+        }
+
+    ]
+}
+
+export default data;
